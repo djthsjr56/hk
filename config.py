84458c3e5aa7e33cb66cd5ca0b512b2e57bef357
djthsjr56 <126954358+djthsjr56@users.noreply.github.com>
2023-05-01 14:07:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
 
-    APP_ID = 
+    APP_ID = 12488427
     
-    API_HASH = ""
+    API_HASH = "cef695e1719a0f7833574f44c7715482"
 
-    ALIVE_NAME = ""
+    ALIVE_NAME = "𝟬𝟬:𝟬𝟬 I𝓎𝒶𝒹 ،🤤)""
 
-    DB_URI = ""
+    DB_URI = "mongodb+srv://belamriiyad7:iyad2009@cluster0.gitwp4w.mongodb.net/?retryWrites=true&w=majority"
 
-    STRING_SESSION = ""
+    STRING_SESSION = "1BJWap1sBuxb6uk03NOGdjUdoqI4LbHP5T5GlQGecJbijTTkjAuCwnjLeRqQTh22kdQXNLqXtIc8nUwdhj2FrZWAj-9xOxlwrP3loySqMIOi8rgUmrhp-L2DiQftefKyv76RzMzNhAw7nq_JmA7LqoDxE-I05Hy5gmRjNPR-E8sw-w_1vvBmojkTThw_Uk4P_OkgiNcjaIH_jVB0YIngBNzkdKvWodZ2cESCiUGUSyqolAGzv0C55dhc5TavN44V9F5HFhZLWaZDghtk6MNFXMZ4HrBiK7Y-VWmUhrpA_nRJ3L0mN-MpfsqojjUj2cmbRClLjTXcNUCIilGRBZL5vSW6iJ9dPDlE="
 
-    TG_BOT_TOKEN = ""
+    TG_BOT_TOKEN = "5955685095:AAEFdM608Dz8NwpGBTtGnUFjEWBo8d4Vx1I"
 
-    PRIVATE_GROUP_BOT_API_ID = 
+    PRIVATE_GROUP_BOT_API_ID = -683584496
 
     COMMAND_HAND_LER = "."
 
